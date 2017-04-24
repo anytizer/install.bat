f5be45bf71fc27d77202a2f85e9c31ab6a3baf64
bimal <gist@bimal.org.np>
2017-04-23 22:00:38 -0600

    apigen

--- install-apigen.bat
+++ install-apigen.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+IF NOT EXIST phar MKDIR phar
 REM curl -L -o apigen.phar http://apigen.org/apigen.phar
-curl -L -O https://github.com/ApiGen/ApiGen.github.io/raw/master/apigen.phar
+curl -L -o phar/apigen.phar https://github.com/ApiGen/ApiGen.github.io/raw/master/apigen.phar
 PAUSE

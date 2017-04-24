0af9908037cafbb79240a6315cd09e94c64269ba
bimal <gist@bimal.org.np>
2017-04-23 21:53:08 -0600

    install angularjs 1.6.4

---
+++ install-angularjs.bat
@@ -0,0 +1,47 @@
+@ECHO OFF
+IF NOT EXIST angularjs MKDIR angularjs
+
+REM Base URL: https://code.angularjs.org/1.6.4/
+
+curl -L -o angularjs/angular-animate.js https://code.angularjs.org/1.6.4/angular-animate.js
+curl -L -o angularjs/angular-animate.min.js https://code.angularjs.org/1.6.4/angular-animate.min.js
+curl -L -o angularjs/angular-animate.min.js.map https://code.angularjs.org/1.6.4/angular-animate.min.js.map
+curl -L -o angularjs/angular-aria.js https://code.angularjs.org/1.6.4/angular-aria.js
+curl -L -o angularjs/angular-aria.min.js https://code.angularjs.org/1.6.4/angular-aria.min.js
+curl -L -o angularjs/angular-aria.min.js.map https://code.angularjs.org/1.6.4/angular-aria.min.js.map
+curl -L -o angularjs/angular-cookies.js https://code.angularjs.org/1.6.4/angular-cookies.js
+curl -L -o angularjs/angular-cookies.min.js https://code.angularjs.org/1.6.4/angular-cookies.min.js
+curl -L -o angularjs/angular-cookies.min.js.map https://code.angularjs.org/1.6.4/angular-cookies.min.js.map
+curl -L -o angularjs/angular-csp.css https://code.angularjs.org/1.6.4/angular-csp.css
+curl -L -o angularjs/angular-loader.js https://code.angularjs.org/1.6.4/angular-loader.js
+curl -L -o angularjs/angular-loader.min.js https://code.angularjs.org/1.6.4/angular-loader.min.js
+curl -L -o angularjs/angular-loader.min.js.map https://code.angularjs.org/1.6.4/angular-loader.min.js.map
+curl -L -o angularjs/angular-message-format.js https://code.angularjs.org/1.6.4/angular-message-format.js
+curl -L -o angularjs/angular-message-format.min.js https://code.angularjs.org/1.6.4/angular-message-format.min.js
+curl -L -o angularjs/angular-message-format.min.js.map https://code.angularjs.org/1.6.4/angular-message-format.min.js.map
+curl -L -o angularjs/angular-messages.js https://code.angularjs.org/1.6.4/angular-messages.js
+curl -L -o angularjs/angular-messages.min.js https://code.angularjs.org/1.6.4/angular-messages.min.js
+curl -L -o angularjs/angular-messages.min.js.map https://code.angularjs.org/1.6.4/angular-messages.min.js.map
+curl -L -o angularjs/angular-mocks.js https://code.angularjs.org/1.6.4/angular-mocks.js
+curl -L -o angularjs/angular-parse-ext.js https://code.angularjs.org/1.6.4/angular-parse-ext.js
+curl -L -o angularjs/angular-parse-ext.min.js https://code.angularjs.org/1.6.4/angular-parse-ext.min.js
+curl -L -o angularjs/angular-parse-ext.min.js.map https://code.angularjs.org/1.6.4/angular-parse-ext.min.js.map
+curl -L -o angularjs/angular-resource.js https://code.angularjs.org/1.6.4/angular-resource.js
+curl -L -o angularjs/angular-resource.min.js https://code.angularjs.org/1.6.4/angular-resource.min.js
+curl -L -o angularjs/angular-resource.min.js.map https://code.angularjs.org/1.6.4/angular-resource.min.js.map
+curl -L -o angularjs/angular-route.js https://code.angularjs.org/1.6.4/angular-route.js
+curl -L -o angularjs/angular-route.min.js https://code.angularjs.org/1.6.4/angular-route.min.js
+curl -L -o angularjs/angular-route.min.js.map https://code.angularjs.org/1.6.4/angular-route.min.js.map
+curl -L -o angularjs/angular-sanitize.js https://code.angularjs.org/1.6.4/angular-sanitize.js
+curl -L -o angularjs/angular-sanitize.min.js https://code.angularjs.org/1.6.4/angular-sanitize.min.js
+curl -L -o angularjs/angular-sanitize.min.js.map https://code.angularjs.org/1.6.4/angular-sanitize.min.js.map
+curl -L -o angularjs/angular-scenario.js https://code.angularjs.org/1.6.4/angular-scenario.js
+curl -L -o angularjs/angular-touch.js https://code.angularjs.org/1.6.4/angular-touch.js
+curl -L -o angularjs/angular-touch.min.js https://code.angularjs.org/1.6.4/angular-touch.min.js
+curl -L -o angularjs/angular-touch.min.js.map https://code.angularjs.org/1.6.4/angular-touch.min.js.map
+curl -L -o angularjs/angular.js https://code.angularjs.org/1.6.4/angular.js
+curl -L -o angularjs/angular.min.js https://code.angularjs.org/1.6.4/angular.min.js
+curl -L -o angularjs/angular.min.js.map https://code.angularjs.org/1.6.4/angular.min.js.map
+curl -L -o angularjs/errors.json https://code.angularjs.org/1.6.4/errors.json
+curl -L -o angularjs/version.json https://code.angularjs.org/1.6.4/version.json
+curl -L -o angularjs/version.txt https://code.angularjs.org/1.6.4/version.txt
